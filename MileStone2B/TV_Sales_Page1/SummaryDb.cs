c57b08e6d33f41558f2f2aec0cb83ccb733ba4fe
Dave Tetreault <davetetreault221@gmail.com>
2013-11-12 09:37:09 -0500

    Broken Stuff :)

--- MileStone2B/TV_Sales_Page1/SummaryDb.cs
+++ MileStone2B/TV_Sales_Page1/SummaryDb.cs
@@ -1,3 +1,4 @@
+using System;
 namespace TV_Sales_Page1
 {
     partial class SummaryDbDataContext
@@ -7,15 +8,25 @@ namespace TV_Sales_Page1
     partial class Summary
     {
 
-        public Summary(string n, int o, string u,decimal st, decimal p, decimal g, decimal t, string d)
+        public Summary(string n, int o, string u,decimal st, decimal p, decimal g, decimal t, DateTime d, 
+            string fn, string ln, string ad, string c, char pr, char pc, int pn, string em)
         {
             Username = u;
             OrderId = o;
-            SubTotal = st;
-            PST = p;
-            GST = g;
-            Total = t;
+            SubTotal = (decimal)st;
+            PST = (decimal)p;
+            GST = (decimal)g;
+            Total = (decimal)t;
             OrderDate = d;
+
+            FirstName = fn;
+            LastName = ln;
+            Address = ad;
+            City = c;
+            Province = pr;
+            PostalCode = pc;
+            Phone = pn;
+            Email = em;
 
         }
     }
